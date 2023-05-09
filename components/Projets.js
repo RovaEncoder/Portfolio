b271eafdc7b4ff12f7cc5471bad951dcc7299ab8
RovaEncoder <abessolochrist30@gmail.com>
2023-05-09 14:43:42 +0200

    fix: update projet and skills component

--- components/Projets.js
+++ components/Projets.js
@@ -2,79 +2,111 @@ import Image from "next/image";
 import Link from "next/link";
 import projet1 from "/public/projet1.png";
 import projet2 from "/public/projet2.png";
+import projet3 from "/public/nest.png";
 
 export default function Projets() {
-	return (
-		<>
-			<div
-				id="Projets"
-				className="w-full  px-3 md:flex flex-col items-center justify-center md:pt-40 max-[767px]:py-40 "
-			>
-				<h2 className="text-white max-[767px]:text-center">Projets récents</h2>
-				<div className="flex flex-col  w-full  gap-y-8  items-center justify-center  max-[767px]:grid-rows-2  ">
-					<div className=" md:flex max-[767px]:flex-col items-center md:w-[80%] w-full   text-white md:px-4">
-						<div className="flex  flex-col md:ml-10 md:w-2/3 w-full  justify-center  ">
-							<p className="md:text-2xl text-xl mb-4 max-[767px]:text-blue-500 ">
-								Projet Webtech
-							</p>
-							<div className=" w-full  p-2 bg-gray-600 opacity-60 hover:opacity-100  max-[767px]:sm ">
-								<p className="text-white max-[767px]:text-justify ">
-									Developpement d'un blog: ce projet scolaire m'a aidé à
-									découvrir un framework très riche et puissant de ReactJs
-									—NextJs. Aussi, j'ai été surpris par la richesse de Supabase —
-									qui couvre l'authentification et l'autorisation, le stockage
-									de fichiers, le stockage de données, les notifications temps
-									réel sur mes données.
-								</p>
-							</div>
-						</div>
-						<div className="md:w-2/3 w-full  flex items-center justify-center md:p-3 my-6 ">
-							<Link href="https://school-project-blog-rova.vercel.app/">
-								<Image
-									className="rounded-xl border border-white/50 shadow-xl shadow-white/20 hover:scale-105 ease-in duration-300 cursor-pointer "
-									src={projet1}
-									width={460}
-									height={460}
-									alt="roro"
-								></Image>
-							</Link>
-						</div>
-					</div>
-					<div className="flex items-center md:w-[80%] w-full max-[767px]:flex-col text-white md:px-4 md:mt-5  ">
-						<div className="md:w-2/3 w-full h-full flex items-center justify-center md:p-3 max-[767px]:order-last  max-[767px]:mt-7">
-							<Link href="https://github.com/RovaEncoder/DevopsGroup-">
-								<Image
-									className="rounded-xl border border-white/50 shadow-xl shadow-white/20 bg-white/70 hover:scale-105 ease-in duration-300 cursor-pointer"
-									src={projet2}
-									width={460}
-									height={460}
-									alt="roro"
-								></Image>
-							</Link>
-						</div>
-						<div className="md:w-2/3 w-full  h-full flex items-center justify-center md:p-3 max-[767px]:my-3">
-							<div className="flex  flex-col w-full h-full justify-center items-end ">
-								<p className="md:text-2xl mb-4 text-xl max-[767px]:text-blue-500">
-									Projet Devops
-								</p>
-								<div className="w-full p-2 bg-gray-600 opacity-60 hover:opacity-100 max-[767px]:sm ">
-									<p className="text-white  max-[767px]:text-justify ">
-										Au cours de ce projet, j'ai acquis des connaissances solides
-										sur la culture DevOps, qui vise à fournir des logiciels de
-										haute qualité aux clients ou utilisateurs de manière rapide
-										et efficace en utilisant l'automatisation pour accélérer et
-										améliorer le processus de développement, de test et de
-										déploiement de logiciels via la livraison continue. Cela
-										garantit que les logiciels sont de qualité, sécurisés et
-										stables, ce qui est essentiel pour répondre aux besoins des
-										clients de manière rapide et fiable.
-									</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div
+        id="Projets"
+        className="w-full  px-3 md:flex flex-col items-center justify-center md:pt-40 max-[767px]:py-40 "
+      >
+        <h2 className="text-white max-[767px]:text-center">Projets récents</h2>
+        <div className="flex flex-col  w-full  gap-y-8  items-center justify-center  max-[767px]:grid-rows-2  ">
+          <div className=" md:flex max-[767px]:flex-col items-center md:w-[80%] w-full   text-white md:px-4">
+            <div className="flex  flex-col md:ml-10 md:w-2/3 w-full  justify-center  ">
+              <p className="md:text-2xl text-xl mb-4 max-[767px]:text-blue-500 ">
+                Projet Webtech
+              </p>
+              <div className=" w-full  p-2 bg-gray-600 opacity-60 hover:opacity-100  max-[767px]:sm ">
+                <p className="text-white max-[767px]:text-justify ">
+                  Developpement d'un blog: ce projet scolaire m'a aidé à
+                  découvrir un framework très riche et puissant de ReactJs
+                  —NextJs. Aussi, j'ai été surpris par la richesse de Supabase —
+                  qui couvre l'authentification et l'autorisation, le stockage
+                  de fichiers, le stockage de données, les notifications temps
+                  réel sur mes données.
+                </p>
+              </div>
+            </div>
+            <div className="md:w-2/3 w-full  flex items-center justify-center md:p-3 my-6 ">
+              <Link href="https://school-project-blog-rova.vercel.app/">
+                <Image
+                  className="rounded-xl border border-white/50 shadow-xl shadow-white/20 hover:scale-105 ease-in duration-300 cursor-pointer "
+                  src={projet1}
+                  width={460}
+                  height={460}
+                  alt="roro"
+                ></Image>
+              </Link>
+            </div>
+          </div>
+          <div className="flex items-center md:w-[80%] w-full max-[767px]:flex-col text-white md:px-4 md:mt-5  ">
+            <div className="md:w-2/3 w-full h-full flex items-center justify-center md:p-3 max-[767px]:order-last  max-[767px]:mt-7">
+              <Link href="https://github.com/RovaEncoder/DevopsGroup-">
+                <Image
+                  className="rounded-xl border border-white/50 shadow-xl shadow-white/20 bg-white/70 hover:scale-105 ease-in duration-300 cursor-pointer"
+                  src={projet2}
+                  width={460}
+                  height={460}
+                  alt="roro"
+                ></Image>
+              </Link>
+            </div>
+            <div className="md:w-2/3 w-full  h-full flex items-center justify-center md:p-3 max-[767px]:my-3">
+              <div className="flex  flex-col w-full h-full justify-center items-end ">
+                <p className="md:text-2xl mb-4 text-xl max-[767px]:text-blue-500">
+                  Projet Devops
+                </p>
+                <div className="w-full p-2 bg-gray-600 opacity-60 hover:opacity-100 max-[767px]:sm ">
+                  <p className="text-white  max-[767px]:text-justify ">
+                    Au cours de ce projet, j'ai acquis des connaissances solides
+                    sur la culture DevOps, qui vise à fournir des logiciels de
+                    haute qualité aux clients ou utilisateurs de manière rapide
+                    et efficace en utilisant l'automatisation pour accélérer et
+                    améliorer le processus de développement, de test et de
+                    déploiement de logiciels via la livraison continue. Cela
+                    garantit que les logiciels sont de qualité, sécurisés et
+                    stables, ce qui est essentiel pour répondre aux besoins des
+                    clients de manière rapide et fiable.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className=" md:flex max-[767px]:flex-col items-center md:w-[80%] w-full   text-white md:px-4">
+            <div className="flex  flex-col md:ml-10 md:w-2/3 w-full  justify-center  ">
+              <p className="md:text-2xl text-xl mb-4 max-[767px]:text-blue-500 text-red-600 ">
+                Projet NestJS
+              </p>
+              <div className=" w-full  p-2 bg-gray-600 opacity-60 hover:opacity-100  max-[767px]:sm ">
+                <p className="text-white max-[767px]:text-justify ">
+                  J'ai développé une API CRUD avancée avec NestJS, incluant
+                  l'enregistrement, l'authentification et la recherche de
+                  produits via l'API OpenFoodFacts, ainsi qu'une mise en cache
+                  pour améliorer les performances. Le projet a été dockerisé.
+                  NestJS, une plateforme pour Node.js, a permis de développer
+                  une API puissante et modulaire, facilement adaptable à divers
+                  environnements de production. Cette expérience a été très
+                  enrichissante pour découvrir les avantages de NestJS pour les
+                  API performantes et robustes.
+                </p>
+              </div>
+            </div>
+            <div className="md:w-2/3 w-full  flex items-center justify-center md:p-3 my-6 ">
+              <Link href="https://github.com/RovaEncoder/api-crud-nestjs">
+                <Image
+                  className="rounded-xl border border-white/50 shadow-xl shadow-white/20 hover:scale-105 ease-in duration-300 cursor-pointer "
+                  src={projet3}
+                  width={460}
+                  height={460}
+                  alt="roro"
+                ></Image>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- components/Skills.js
+++ components/Skills.js
@@ -1,98 +1,97 @@
 import {
-	FaHtml5,
-	FaCss3,
-	FaReact,
-	FaNodeJs,
-	FaPython,
-	FaGitAlt,
+  FaHtml5,
+  FaCss3,
+  FaReact,
+  FaNodeJs,
+  FaPython,
+  FaGitAlt,
 } from "react-icons/fa";
 import {
-	SiJavascript,
-	SiNextdotjs,
-	SiScala,
-	SiMongodb,
-	SiMysql,
-	SiCsharp,
-	SiPhp,
-	SiVagrant,
-	SiDocker,
-	SiKubernetes,
+  SiJavascript,
+  SiNextdotjs,
+  SiScala,
+  SiMongodb,
+  SiMysql,
+  SiNestjs,
+  SiVagrant,
+  SiDocker,
+  SiKubernetes,
 } from "react-icons/si";
 import { GrHadoop } from "react-icons/gr";
 
 export default function Skills() {
-	return (
-		<>
-			<div
-				id="Skills"
-				className="my-100 w-full h-full flex items-center justify-center flex-col  pt-40 pb-5 "
-			>
-				<p className="text-3xl font-semibold mb-10 text-white">
-					Langages & Outils
-				</p>
-				<div className="grid md:gap-x-20 gap-x-5 md:gap-y-4 gap-y-8 grid-cols-3   w-[80%] p-2">
-					<div className="bg-white p-3 flex flex-col justify-center  items-center text-orange-500 shadow-md shadow-orange-500 hover:opacity-100 rounded-md hover:scale-105 ease-in duration-150">
-						<p>HML</p>
-						<FaHtml5 size={35} />
-					</div>
-					<div className="bg-white p-3 flex flex-col justify-center items-center text-blue-800 shadow-md shadow-blue-800 hover:opacity-100 rounded-md hover:scale-105 ease-in duration-150">
-						<p>CSS</p>
-						<FaCss3 size={35} />
-					</div>
-					<div className="bg-white p-3 flex flex-col justify-center items-center text-yellow-500 shadow-md shadow-yellow-500  hover:opacity-100 rounded-md hover:scale-105 ease-in duration-150">
-						<p>Javascript</p>
-						<SiJavascript size={35} />
-					</div>{" "}
-					<div className="bg-white text-black p-3 flex flex-col justify-center items-center text-blue-150 shadow-md shadow-blue-150 hover:opacity-100 rounded-md hover:scale-105 ease-in duration-150">
-						<p>React</p>
-						<FaReact size={35} />
-					</div>
-					<div className="bg-white text-black p-3 flex flex-col justify-center items-center shadow-md shadow-yellow-800 ty-50 hover:opacity-100 rounded-md  hover:scale-105 ease-in duration-150">
-						<p>NextJs</p>
-						<SiNextdotjs size={35} />
-					</div>
-					<div className="bg-white p-3 flex flex-col justify-center items-center text-green-800 shadow-md shadow-green-800  hover:opacity-100 rounded-md  hover:scale-105 ease-in duration-150">
-						<p>NodeJs</p>
-						<FaNodeJs size={35} />
-					</div>
-					<div className="bg-white p-3 text-black flex flex-col justify-center items-center  hover:opacity-100 rounded-md  hover:scale-105 ease-in duration-150">
-						<p>Python</p>
-						<FaPython size={35} />
-					</div>
-					<div className="bg-white p-3 flex flex-col justify-center items-center text-red-600  shadow-md shadow-red-600 hover:opacity-100 rounded-md  hover:scale-105 ease-in duration-150">
-						<p>Scala</p>
-						<SiScala size={35} />
-					</div>
-					<div className="bg-white p-3 flex flex-col justify-center items-center text-yellow-800 shadow-md shadow-yellow-800  hover:opacity-100 rounded-md  hover:scale-105 ease-in duration-150">
-						<p>Hadoop</p>
-						<GrHadoop size={35} />
-					</div>
-					<div className="bg-white p-3 flex flex-col justify-center items-center text-orange-600 shadow-md shadow-orange-600 hover:opacity-100 rounded-md  hover:scale-105 ease-in duration-150">
-						<p>Git</p>
-						<FaGitAlt size={35} />
-					</div>
-					<div className="bg-white p-3 flex flex-col justify-center text-black items-center shadow-md shadow-yellow-800 hover:opacity-100 rounded-md hover:scale-105 ease-in duration-150">
-						<p>SQL</p>
-						<SiMysql size={35} />
-					</div>
-					<div className="bg-white p-3 flex flex-col justify-center items-center text-green-800 shadow-md shadow-green-800  hover:opacity-100 rounded-md hover:scale-105 ease-in duration-150">
-						<p>MongoDB</p>
-						<SiMongodb size={35} />
-					</div>
-					<div className="bg-white p-3 flex flex-col justify-center items-center text-blue-600 shadow-md shadow-blue-600  hover:opacity-100 rounded-md hover:scale-105 ease-in duration-150">
-						<p>Vagrant</p>
-						<SiVagrant size={35} />
-					</div>{" "}
-					<div className="bg-white p-3 flex flex-col justify-center items-center text-blue-600 shadow-md shadow-blue-600  hover:opacity-100 rounded-md hover:scale-105 ease-in duration-150">
-						<p>Docker</p>
-						<SiDocker size={35} />
-					</div>
-					<div className="bg-white p-3 flex flex-col justify-center items-center text-blue-600 shadow-md shadow-blue-600    hover:opacity-100 rounded-md hover:scale-105 ease-in duration-150">
-						<p>Kubernetes</p>
-						<SiKubernetes size={35} />
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div
+        id="Skills"
+        className="my-100 w-full h-full flex items-center justify-center flex-col  pt-40 pb-5 "
+      >
+        <p className="text-3xl font-semibold mb-10 text-white">
+          Langages & Outils
+        </p>
+        <div className="grid md:gap-x-20 gap-x-5 md:gap-y-4 gap-y-8 grid-cols-3   w-[80%] p-2">
+          <div className="bg-white p-3 flex flex-col justify-center  items-center text-orange-500 shadow-md shadow-orange-500 hover:opacity-100 rounded-md hover:scale-105 ease-in duration-150">
+            <p>HML</p>
+            <FaHtml5 size={35} />
+          </div>
+          <div className="bg-white p-3 flex flex-col justify-center items-center text-blue-800 shadow-md shadow-blue-800 hover:opacity-100 rounded-md hover:scale-105 ease-in duration-150">
+            <p>CSS</p>
+            <FaCss3 size={35} />
+          </div>
+          <div className="bg-white p-3 flex flex-col justify-center items-center text-yellow-500 shadow-md shadow-yellow-500  hover:opacity-100 rounded-md hover:scale-105 ease-in duration-150">
+            <p>Javascript</p>
+            <SiJavascript size={35} />
+          </div>{" "}
+          <div className="bg-white text-black p-3 flex flex-col justify-center items-center text-blue-150 shadow-md shadow-blue-150 hover:opacity-100 rounded-md hover:scale-105 ease-in duration-150">
+            <p>React</p>
+            <FaReact size={35} />
+          </div>
+          <div className="bg-white text-black p-3 flex flex-col justify-center items-center shadow-md shadow-yellow-800 ty-50 hover:opacity-100 rounded-md  hover:scale-105 ease-in duration-150">
+            <p>NextJs</p>
+            <SiNextdotjs size={35} />
+          </div>
+          <div className="bg-white p-3 flex flex-col justify-center items-center text-green-800 shadow-md shadow-green-800  hover:opacity-100 rounded-md  hover:scale-105 ease-in duration-150">
+            <p>NodeJs</p>
+            <FaNodeJs size={35} />
+          </div>
+          <div className="bg-white p-3 text-red-600 flex flex-col justify-center items-center  hover:opacity-100 rounded-md  hover:scale-105 ease-in duration-150">
+            <p>NextJs</p>
+            <SiNestjs size={35} />
+          </div>
+          <div className="bg-white p-3 text-black flex flex-col justify-center items-center  hover:opacity-100 rounded-md  hover:scale-105 ease-in duration-150">
+            <p>Python</p>
+            <FaPython size={35} />
+          </div>
+          <div className="bg-white p-3 flex flex-col justify-center items-center text-red-600  shadow-md shadow-red-600 hover:opacity-100 rounded-md  hover:scale-105 ease-in duration-150">
+            <p>Scala</p>
+            <SiScala size={35} />
+          </div>
+          <div className="bg-white p-3 flex flex-col justify-center items-center text-yellow-800 shadow-md shadow-yellow-800  hover:opacity-100 rounded-md  hover:scale-105 ease-in duration-150">
+            <p>Hadoop</p>
+            <GrHadoop size={35} />
+          </div>
+          <div className="bg-white p-3 flex flex-col justify-center items-center text-orange-600 shadow-md shadow-orange-600 hover:opacity-100 rounded-md  hover:scale-105 ease-in duration-150">
+            <p>Git</p>
+            <FaGitAlt size={35} />
+          </div>
+          <div className="bg-white p-3 flex flex-col justify-center text-black items-center shadow-md shadow-yellow-800 hover:opacity-100 rounded-md hover:scale-105 ease-in duration-150">
+            <p>SQL</p>
+            <SiMysql size={35} />
+          </div>
+          <div className="bg-white p-3 flex flex-col justify-center items-center text-green-800 shadow-md shadow-green-800  hover:opacity-100 rounded-md hover:scale-105 ease-in duration-150">
+            <p>MongoDB</p>
+            <SiMongodb size={35} />
+          </div>
+          <div className="bg-white p-3 flex flex-col justify-center items-center text-blue-600 shadow-md shadow-blue-600  hover:opacity-100 rounded-md hover:scale-105 ease-in duration-150">
+            <p>Docker</p>
+            <SiDocker size={35} />
+          </div>
+          <div className="bg-white p-3 flex flex-col justify-center items-center text-blue-600 shadow-md shadow-blue-600    hover:opacity-100 rounded-md hover:scale-105 ease-in duration-150">
+            <p>Kubernetes</p>
+            <SiKubernetes size={35} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
